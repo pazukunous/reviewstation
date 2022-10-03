0b549a5d31f93ac7aac6ea9b3677c54b39213543
Chris Mann <chrispmann@gmail.com>
2022-10-03 11:20:19 -0700

    searching returns results to console.log

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Search(){
+
+    // sets state of movie search
+    const[searchTerm, setSearchTerm] = useState("")
+
+
+
+//function to only search once the user hasn't typed for a bit
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+        console.log(searchTerm)
+        }, 3000)
+        
+        return () => clearTimeout(delayDebounceFn)
+    }, [searchTerm])
+// function to handle search and create suggestions for movie titles
+    function handleSearch(event){
+        event.preventDefault()
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${searchTerm}`)
+            .then(resource => resource.json())
+            .then(movieSuggestions => (console.log(movieSuggestions)))
+    
+    console.log(event)
+    setSearchTerm(event.target.value)
+
+   
+    }
+
     return(
         <div className="searchBar">
-            <form>
             <input
             type="text"
             placeholder="Search for a film.."
+            value = {searchTerm}
+            onChange = {handleSearch}
             />
-            <input
-            type="submit"
-            value="Submit"
-            />
-            </form>
+           
+           
         </div>
     )
 }
